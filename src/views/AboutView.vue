804733d3a1fa00429d75f7e5f50c79d3a8f60285
TheXName <markkap0807@gmail.com>
2025-09-26 18:56:54 +0200

    add vue-router with basic views

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h1>About</h1>
+    <p>This website presents sensors and devices for PLC equipment.</p>
+  </section>
+</template>
+
+<script>
+export default { name: 'AboutView' }
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h1>Welcome to PLC Parts Catalog</h1>
+  </section>
+</template>
+
+<script>
+export default { name: 'HomeView' }
+</script>
