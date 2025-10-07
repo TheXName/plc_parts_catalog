469739205133dd19114220b631593e6739ffe1cc
TheXName <markkap0807@gmail.com>
2025-10-07 21:27:40 +0200

    Prettier has been added

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -3,11 +3,7 @@
     <h1 class="mb-4">Catalog</h1>
 
     <v-row>
-      <v-col
-        v-for="item in products"
-        :key="item.id"
-        cols="12" sm="6" md="4" lg="3"
-      >
+      <v-col v-for="item in products" :key="item.id" cols="12" sm="6" md="4" lg="3">
         <EquipmentCard :item="item" :is-favorite="false" />
       </v-col>
     </v-row>
