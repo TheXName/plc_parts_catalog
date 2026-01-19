8376643bae3175301ef513fa102072da18c41b64
TheXName <markkap0807@gmail.com>
2026-01-19 20:25:00 +0300

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/plc_parts_catalog/',
-
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
